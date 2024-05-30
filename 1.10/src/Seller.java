5cea941cfd9eda59ae67344e5192b308a2681123
Daniel Acai <daniel.aci.acai@gmail.com>
2024-05-30 13:02:38 +0200

    Added seller class with all attributes!

---
+++ 1.10/src/Seller.java
@@ -0,0 +1,14 @@
+import java.net.InetAddress;
+import java.time.LocalDate;
+
+public class Seller {
+
+    String name;
+    String surname;
+    LocalDate birthDate;
+    double soldCarrots;             // in tons
+    String headquarters;
+    String VRI;                     // vehicle registration identifier
+    double vehicleConsumption;      // in l/100km
+    String IPv4;                    // also possible to use *InetAddress IPv4*
+}
